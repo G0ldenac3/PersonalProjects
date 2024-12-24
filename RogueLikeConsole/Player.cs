7a56543910d36da83647954be1c92b628d3d085e
Vantuyne Jasper <vantuynejasperb@gmail.com>
2024-12-24 14:40:40 +0100

    Started new project roguelikeconsole

---
+++ RogueLikeConsole/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueLikeConsole
+{
+    internal class Player
+    {
+        private int PlayerHealth;
+        public int Health
+        {
+            get { return PlayerHealth; }
+            set { PlayerHealth = value; }
+        }
+    }
+}
---
+++ RogueLikeConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RogueLikeConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player Health = new Player();
+            Health.Health = 100;
+
+            System.Console.WriteLine("type yes");
+            if (System.Console.ReadLine() == "yes")
+            {
+                Health.Health = 50;
+            }
+            else
+            {
+                System.Console.WriteLine("You didn't type yes");
+            }
+
+
+        }
+    }
+}
